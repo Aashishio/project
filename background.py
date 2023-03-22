0d356356e06ffee0c4bfd7aa5308247ecd22894c
Aashishio <98104806+Aashishio@users.noreply.github.com>
2023-03-22 12:30:02 +0530

    Add files via upload

---
+++ background.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import random
+
+root = Tk()
+
+root.geometry("600x600")
+root.title("Random Backgroud colour")
+
+random_colours = {
+    "1":"orange",
+    "2":"blue",
+    "3":"red",
+    "4":"white",
+    "5":"yellow",
+    "6":"purple",
+    "7":"green",
+    "8":"black",
+    "9":"pink",
+    "10":"grey",
+    "11":"brown",
+    "12":"pink",
+    "13":"purple",    
+}
+
+
+def Random():
+    random_index = random.randint(0, 13)
+    random_colour = random_colours["random_index"]
+    root.configure(bg=random_colour)
+
+
+btn = Button(root, text="Click here for random background" ,bg="black", fg="white", command=Random)
+btn.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+root.mainloop()
