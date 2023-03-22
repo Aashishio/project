90b3f6ded3d8c4427a1a2f96cceaa601c9506288
Aashishio <98104806+Aashishio@users.noreply.github.com>
2023-03-22 18:15:30 +0530

    Add files via upload

--- background.py
+++ background.py
@@ -7,25 +7,25 @@ root.geometry("600x600")
 root.title("Random Backgroud colour")
 
 random_colours = {
-    "1":"orange",
-    "2":"blue",
-    "3":"red",
-    "4":"white",
-    "5":"yellow",
-    "6":"purple",
-    "7":"green",
-    "8":"black",
-    "9":"pink",
-    "10":"grey",
-    "11":"brown",
-    "12":"pink",
-    "13":"purple",    
+    1:"orange",
+    2:"blue",
+    3:"red",
+    4:"white",
+    5:"yellow",
+    6:"purple",
+    7:"green",
+    8:"black",
+    9:"pink",
+    10:"grey",
+    11:"brown",
+    12:"pink",
+    13:"purple",    
 }
 
 
 def Random():
     random_index = random.randint(0, 13)
-    random_colour = random_colours["random_index"]
+    random_colour = random_colours[random_index]
     root.configure(bg=random_colour)
 
 
